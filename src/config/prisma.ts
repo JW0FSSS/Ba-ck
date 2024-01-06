9c6d5ea9843142027f8e17ef1b01c1e27d6455ea
Jose <dstryr77@gmail.com>
2024-01-06 09:53:56 -0500

    working with docker

---
+++ src/config/prisma.ts
@@ -0,0 +1,8 @@
+import {PrismaClient} from '@prisma/client'
+
+export const prisma= new PrismaClient()
+
+export async function main() {
+    const result = await prisma.$queryRaw`SELECT 1 + 1 `
+    return result  
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
 import express,{Request,Response} from 'express'
 import './config/enviroment.ts'
+import { main, prisma } from './config/prisma.js'
 
 const app = express()
 
 app.use((req:Request,res:Response)=>{
     res.status(404).end('Error 404')
 })
+
+main()
+  .then(async (data) => {
+    console.log(data);
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
 
 app.listen(process.env.PORT,()=>console.log(`Turning on server on http://localhost:${process.env.PORT}`))
